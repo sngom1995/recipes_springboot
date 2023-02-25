cb13500ffecc83ee659d1a88a23a25255781bf88
sngom1995 <ngomsamba75@gmail.com>
2023-02-25 13:47:53 +0000

    add Difficulty enumeration to Recipe

--- src/main/java/sn/springframework/recipes/domain/Recipe.java
+++ src/main/java/sn/springframework/recipes/domain/Recipe.java
@@ -20,6 +20,8 @@ public class Recipe {
     private Set<Ingredient> ingredients ;
     @Lob
     private Byte[] image;
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
@@ -103,5 +105,21 @@ public class Recipe {
 
     public void setImage(Byte[] image) {
         this.image = image;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
