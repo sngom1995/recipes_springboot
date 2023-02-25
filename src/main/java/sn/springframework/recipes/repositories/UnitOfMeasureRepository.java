5261894e5d234bb5726a824945a356110da1c72a
sngom1995 <ngomsamba75@gmail.com>
2023-02-25 16:11:31 +0000

    database initialization

--- src/main/java/sn/springframework/recipes/repositories/UnitOfMeasureRepository.java
+++ src/main/java/sn/springframework/recipes/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,8 @@ package sn.springframework.recipes.repositories;
 import org.springframework.data.repository.CrudRepository;
 import sn.springframework.recipes.domain.UnitOfMeasure;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT into category (description) values ('American');
+INSERT into category (description) values ('Italian');
+INSERT into category (description) values ('Mexican');
+INSERT into category (description) values ('Fast Food');
+INSERT  INTO unit_of_measure (description) VALUES ('Teaspoon');
+INSERT  INTO unit_of_measure (description) VALUES ('Tablespoon');
+INSERT  INTO unit_of_measure (description) VALUES ('Cup');
+INSERT  INTO unit_of_measure (description) VALUES ('Pinch');
+INSERT  INTO unit_of_measure (description) VALUES ('Ounce');
+
